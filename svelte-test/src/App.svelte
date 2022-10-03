77e3187fe83beb98fe23688df60f57bbd495497e
Jeon Won <j2onwon@gmail.com>
2022-10-04 01:19:36 +0900

    20221004_0119

---
+++ svelte-test/src/App.svelte
@@ -0,0 +1,53 @@
+<script>
+	import CustomSelect from "./CustomSelect.svelte";
+
+	let fruit = "apple";
+
+	let color = "yellow";
+	let colorList = [
+		{ code: "red", desc: "빨강" },
+		{ code: "green", desc: "초록" },
+		{ code: "blue", desc: "파랑" },
+		{ code: "yellow", desc: "노랑" }
+	];
+	let param = {
+		choice: "red",
+		list: [
+			{ code: "red", desc: "빨강" },
+			{ code: "green", desc: "초록" },
+			{ code: "blue", desc: "파랑" },
+			{ code: "yellow", desc: "노랑" }
+		]
+	};
+</script>
+
+<main>
+	<ul>
+		<li>1. 하위 컴포넌트를 그대로 사용한 경우: <CustomSelect/></li>
+		<li>개발자 B: <CustomSelect bind:choice={fruit}/> - {fruit} 선택됨</li> <!-- choice props에 fruit 전달 -->
+		<li>개발자 C: <CustomSelect list={colorList} bind:choice={color}/></li>
+		<li>Spread: <CustomSelect {...param}/> - {param.choice} 선택됨</li>
+	</ul>
+</main>
+
+<style>
+	main {
+		/* text-align: center; */
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
---
+++ svelte-using-markdown/src/App.svelte
@@ -0,0 +1,58 @@
+<script>
+	import { marked } from "marked";
+	export let name;
+
+	let mark = `
+Svelte Markdown 테스트
+
+# 제목
+
+**진하게**
+
+_이탤릭_
+
+~~취소선~~
+
+> 인용
+
+[링크텍스트](https://github.com/jeon-won)
+
+![](https://images.velog.io/images/jeonwon/post/b4e4ae50-eabc-4de1-b021-3c20a351edfd/277315121_385607339768767_3361049751274918311_n.jpg)
+
+코드블록은 백틱 문자 3개를 연달아 사용해야 하는데, 자바스크립트 백틱 문자 안에 백틱 문자를 어떻게 표현해야 하는지 모르게씀...
+
+표는 제대로 표시 안 되네...
+
+|ㅁㄴㅇㄹ|ㅁㄴㅇㄹ|
+|------|------|
+|1-----|ㅁ-----|
+`;
+</script>
+
+<main>
+	<h1>Hello {name}!</h1>
+	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	{@html marked(mark)}
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
