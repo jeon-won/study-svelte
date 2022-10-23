ef655959030d227c7ca987c02b77d60a376b7cd8
Jeon Won <j2onwon@gmail.com>
2022-10-24 01:25:57 +0900

    20221024_0125_remove jwt-server

--- jwt-server/test.js
+++
@@ -1,8 +0,0 @@
-let users = [
-  { email: "admin", password: "adminpw" }, 
-  { email: "svelte", password: "sveltepw" }
-]
-
-let user = { email: "admins", password: "adminpw" };
-let userEmail = user.email;
-console.log(users.find(user => user.email === userEmail));
